8fc3c909265122d563616f774daf8b25ebf7f310
MarioTawadrous <mario.tawadrous.95@gmail.com>
2025-07-26 22:53:19 +0300

    Addedd Home and About pages

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <div>
       <h1>Header component</h1>
+      <nav>
+        <ul className="flex gap-1.5">
+          <li>
+            <Link href="/home">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
