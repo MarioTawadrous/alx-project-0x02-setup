f04d5a287ab798a458cc6ac91225645c6785da3d
MarioTawadrous <mario.tawadrous.95@gmail.com>
2025-07-27 18:39:11 +0300

    Updated posts page

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,6 +1,6 @@
 // pages/posts.tsx
 import React from "react";
-import Header from "../components/layout/Header";
+import Header from "@/components/layout/Header";
 
 const PostsPage: React.FC = () => {
   return (
